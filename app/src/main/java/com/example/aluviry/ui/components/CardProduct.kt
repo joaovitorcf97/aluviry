7eafcc6bf344e4657c625a26df178b9c3e9989a7
João Vitor <joaovitorcfdev@gmail.com>
2023-08-20 23:36:16 -0300

    showing description with click

--- app/src/main/java/com/example/aluviry/ui/components/CardProduct.kt
+++ app/src/main/java/com/example/aluviry/ui/components/CardProduct.kt
@@ -1,6 +1,7 @@
 package com.example.aluviry.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,6 +11,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -24,12 +29,19 @@ import com.example.aluviry.model.Product
 fun CardProductItem(
     product: Product,
     modifier: Modifier = Modifier,
-    elevation: Dp = 4.dp
+    elevation: Dp = 4.dp,
+    isExpadend: Boolean = false,
 ) {
+    var expanded by remember {
+        mutableStateOf(isExpadend)
+    }
     Card(
         modifier
             .fillMaxWidth()
-            .heightIn(150.dp),
+            .heightIn(150.dp)
+            .clickable {
+                expanded = !expanded
+            },
         //elevation = elevation,
     ) {
         Column {
@@ -55,14 +67,15 @@ fun CardProductItem(
                     text = product.price.toBrazilCurrency()
                 )
             }
-            product.description?.let {
-                Text(
-                    text = product.description,
-                    Modifier
-                        .padding(16.dp)
-                )
+            if (expanded) {
+                product.description?.let {
+                    Text(
+                        text = product.description,
+                        Modifier
+                            .padding(16.dp)
+                    )
+                }
             }
-
         }
     }
 }
